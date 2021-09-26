5dbca90b622f0a0874aeef790400001163a91641
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 19:56:16 +0530

    Message Should Return Sad

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,25 +1,10 @@
 public class MoodAnalyser
 {
-    private String message;
-    public MoodAnalyser(String message)
+    public String analyseMood(String message)
     {
-        this.message = message;
-    }
-
-    public String analyseMood() throws MoodAnalyserException {
-        try
-        {
-            if(message.length() == 0)
-            {
-                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,"Please enter valid message");
-            }
-            if (message.contains("sad"))
-                return "sad";
-            else
-                return "Happy";
-        } catch (NullPointerException e )
-        {
-            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL,"Please enter valid message");
-        }
+        if(message.contains("sad"))
+            return "sad";
+        else
+            return "Happy";
     }
 }
