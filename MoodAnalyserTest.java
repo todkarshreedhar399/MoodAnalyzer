fa1b7294e3cfe88903eca5dd5e8c801773ea78ae
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 19:34:14 +0530

    Success : Refactoring Using Constructor

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,29 +1,15 @@
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.Assert;
 
 public class MoodAnalyserTest
 {
-    MoodAnalyser moodAnalyser = null;
-
-    @Before
-    public void objectCreation()
-    {
-        moodAnalyser = new MoodAnalyser();
-    }
     @Test
     public void iAmIn_SadMood_ShouldReturn_sad()
     {
-        String mood = moodAnalyser.analyseMood("I am in sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("sad" , mood);
     }
-
-    @Test
-    public void iAmIn_AnyMood_ShouldReturn_Happy()
-    {
-        String mood = moodAnalyser.analyseMood("I am in any mood");
-        Assert.assertEquals("Happy" , mood);
-    }
-
 
 }
